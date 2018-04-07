b53165a56fb7ab4952f1322551da3a88110e4349
AP-STB1\Pinor <Project Creation 2017-3>
2018-04-07 15:47:42 -0400

    Completed Chrono

--- inf8405runner/app/src/main/java/com/example/pinor/inf8405runner/ChronoFragment.java
+++ inf8405runner/app/src/main/java/com/example/pinor/inf8405runner/ChronoFragment.java
@@ -3,68 +3,109 @@ package com.example.pinor.inf8405runner;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import java.util.Timer;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ChronoFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ChronoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ChronoFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    private Button startButton;
+    private Button stopButton;
+
+
+    private long startTime = 0L;
+    private long pauseTime = 0L;
+    private long pauseDelay = 0L;
+    private long stopTime = 0L;
+    long currentTime = 0L;
+
+    private boolean onPause = false;
+    private boolean onStop = true;
+
+    private TextView chronoText;
+
+    private Handler customHandler = new Handler();
+
 
     public ChronoFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChronoFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static ChronoFragment newInstance(String param1, String param2) {
         ChronoFragment fragment = new ChronoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chrono, container, false);
+        View RootView = inflater.inflate(R.layout.fragment_chrono, container, false);
+        chronoText = (TextView) RootView.findViewById(R.id.time_tv);
+
+        startButton = (Button) RootView.findViewById(R.id.start_b);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if (startTime == 0) {
+                    startTime = SystemClock.uptimeMillis();
+                    onStop = false;
+                    Log.d("Chrono", "Started");
+                } else if (!onPause) {
+                    pauseTime = SystemClock.uptimeMillis();
+                    onStop = false;
+                    onPause = true;
+                    Log.d("Chrono", "On pause");
+                } else {
+                    currentTime = SystemClock.uptimeMillis();
+                    pauseDelay += currentTime - pauseTime;
+                    onPause = false;
+                    Log.d("Chrono", "Restarted");
+                }
+                customHandler.postDelayed(updateTimerThread, 0);
+            }
+        });
+
+        stopButton = (Button) RootView.findViewById(R.id.stop_b);
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                stopTime = SystemClock.uptimeMillis();
+                onPause = false;
+                onStop = true;
+                Log.d("Chrono", "Stopped");
+                updateDB();
+                reinitializeTimer();
+                customHandler.postDelayed(updateTimerThread, 0);
+            }
+        });
+
+        return RootView;
+    }
+
+    public void updateDB() {
+
+    }
+
+    public void reinitializeTimer() {
+        startTime = 0L;
+        pauseTime = 0L;
+        pauseDelay = 0L;
+        stopTime = 0L;
+        currentTime = 0L;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -80,8 +121,7 @@ public class ChronoFragment extends Fragment {
         if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
         } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+            throw new RuntimeException(context.toString() + " must implement OnFragmentInteractionListener");
         }
     }
 
@@ -90,6 +130,37 @@ public class ChronoFragment extends Fragment {
         super.onDetach();
         mListener = null;
     }
+
+    private Runnable updateTimerThread = new Runnable() {
+        public void run() {
+            if (onPause) {
+                long chronoValue = pauseTime - startTime - pauseDelay;
+                Log.d("Chrono", "cv = " + chronoValue);
+                int secs = (int) (chronoValue / 1000);
+                int hours = secs / 60 / 60;
+                int mins = secs / 60 - hours * 60;
+                secs = secs % 60;
+
+                chronoText.setText(String.format("%02d", hours) + ":" + String.format("%02d", mins) + ":" + String.format("%02d", secs));
+                startButton.setText("Redémarrer");
+            } else if (onStop) {
+                chronoText.setText("00:00:00");
+                startButton.setText("Démarrer");
+            } else {
+                long deltaTime = SystemClock.uptimeMillis() - startTime - pauseDelay;
+                Log.d("chrono", "" + pauseDelay + ", deltaTime = " + deltaTime);
+                int secs = (int) (deltaTime / 1000);
+                int hours = secs / 60 / 60;
+                int mins = secs / 60 - hours * 60;
+                secs = secs % 60;
+
+                chronoText.setText(String.format("%02d", hours) + ":" + String.format("%02d", mins) + ":" + String.format("%02d", secs));
+                startButton.setText("Pause");
+            }
+            customHandler.postDelayed(this, 0);
+        }
+    };
+
 
     /**
      * This interface must be implemented by activities that contain this
