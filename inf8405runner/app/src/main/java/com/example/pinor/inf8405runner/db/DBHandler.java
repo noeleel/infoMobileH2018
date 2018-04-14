43f9df35dd8e39c000fea40e12ba47b9a637ac90
AP-STB1\Pinor <pinor1618@gmail.com>
2018-04-14 14:50:07 -0400

    Ajout des infos pour utiliser la db pour la batterie

--- inf8405runner/app/src/main/java/com/example/pinor/inf8405runner/db/DBHandler.java
+++ inf8405runner/app/src/main/java/com/example/pinor/inf8405runner/db/DBHandler.java
@@ -24,23 +24,38 @@ public class DBHandler extends SQLiteOpenHelper {
     private static final String TIME_COL_NAME = "time";
     private static final String DISTANCE_COL_NAME = "distance";
 
+    private static final String TABLE_DEVICE_INFO = "device_info";
+    private static final String ID_DI_COL_NAME = "id";
+    private static final String BATTERY_COL_NAME = "battery_level";
+    private static final String TXDEPART_COL_NAME = "tx_depart";
+    private static final String RXDEPART_COL_NAME = "rx_depart";
+
     public DBHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_RESULTS + " (";
-        CREATE_CONTACTS_TABLE += ID_COL_NAME + " INTEGER PRIMARY KEY,";
-        CREATE_CONTACTS_TABLE += TIME_COL_NAME + " TEXT,";
-        CREATE_CONTACTS_TABLE += DISTANCE_COL_NAME + " TEXT" + ")";
-        Log.d("DB",CREATE_CONTACTS_TABLE);
-        db.execSQL(CREATE_CONTACTS_TABLE);
+        String CREATE_RESULTS_TABLE = "CREATE TABLE " + TABLE_RESULTS + " (";
+        CREATE_RESULTS_TABLE += ID_COL_NAME + " INTEGER PRIMARY KEY, ";
+        CREATE_RESULTS_TABLE += TIME_COL_NAME + " TEXT, ";
+        CREATE_RESULTS_TABLE += DISTANCE_COL_NAME + " TEXT" + ")";
+        Log.d("DB",CREATE_RESULTS_TABLE);
+        db.execSQL(CREATE_RESULTS_TABLE);
+
+        String CREATE_DEVICE_TABLE = "CREATE TABLE " + TABLE_DEVICE_INFO + " (";
+        CREATE_DEVICE_TABLE += ID_DI_COL_NAME + " INTEGER PRIMARY KEY, ";
+        CREATE_DEVICE_TABLE += BATTERY_COL_NAME + " INTEGER, ";
+        CREATE_DEVICE_TABLE += TXDEPART_COL_NAME + " INTEGER, ";
+        CREATE_DEVICE_TABLE += RXDEPART_COL_NAME + " INTEGER" + ")";
+        Log.d("DB",CREATE_DEVICE_TABLE);
+        db.execSQL(CREATE_DEVICE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_RESULTS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_DEVICE_INFO);
         onCreate(db);
     }
 
@@ -73,6 +88,52 @@ public class DBHandler extends SQLiteOpenHelper {
             } while (cursor.moveToNext());
         }
         return resultsList;
+    }
+
+    public void insertDeviceInfo(DeviceInfo deviceInfo) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues row = new ContentValues();
+        row.put(BATTERY_COL_NAME, deviceInfo.get_batteryLevel());
+        row.put(TXDEPART_COL_NAME, deviceInfo.get_txDepart());
+        row.put(RXDEPART_COL_NAME, deviceInfo.get_rxDepart());
+
+        db.insert(TABLE_DEVICE_INFO, null, row);
+        db.close();
+    }
+
+    public void clearAllDeviceInfo() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String deleteSQL = "DELETE FROM " + TABLE_DEVICE_INFO;
+        db.execSQL(deleteSQL);
+    }
+
+    public DeviceInfo getDeviceInfo() {
+        // List<DeviceInfo> deviceInfoList = new ArrayList<DeviceInfo>();
+
+        String selectQuery = "SELECT * FROM " + TABLE_DEVICE_INFO;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        DeviceInfo info = new DeviceInfo();
+
+        if (cursor.moveToFirst()) {
+            //do {
+
+            info.set_id(cursor.getInt(0));
+            info.set_batteryLevel(cursor.getInt(1));
+            info.set_txDepart(cursor.getInt(2));
+            info.set_rxDepart(cursor.getInt(3));
+
+            //deviceInfoList.add(info);
+            //} while (cursor.moveToNext());
+        }
+        return info;
+    }
+
+    public void updateDeviceInfo(DeviceInfo info) {
+        clearAllDeviceInfo();
+        insertDeviceInfo(info);
     }
 }
 
