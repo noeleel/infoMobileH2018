ccb0a6022b7e42a5bee4b98918bc8c500b94adf3
LAPTOP-UV5JFG6L\Elodie <elodie.noele@ensta-bretagne.org>
2018-04-17 01:49:21 -0400

    no message

--- inf8405runner/app/src/main/java/com/example/pinor/inf8405runner/ProgressionFragment.java
+++ inf8405runner/app/src/main/java/com/example/pinor/inf8405runner/ProgressionFragment.java
@@ -1,71 +1,194 @@
 package com.example.pinor.inf8405runner;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Color;
+import android.location.Location;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ProgressionFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ProgressionFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ProgressionFragment extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+
+import com.example.pinor.inf8405runner.db.DBHandler;
+import com.example.pinor.inf8405runner.db.Run;
+
+
+
+public class ProgressionFragment extends Fragment implements OnMapReadyCallback {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ArrayList<Location> locationList = new ArrayList <> () ;
+    private ArrayList<LatLng> latLngList = new ArrayList <> () ;
+    private Spinner spinner;
+    private Button btnShare;
+    private Button btnView;
+
 
     private OnFragmentInteractionListener mListener;
+    private MapView mapView;
+    private GoogleMap googleMap;
+    private PolylineOptions lineOptions;
+
+    private ArrayList<Run> runs;
+    private DBHandler db;
+    private int index ;
 
     public ProgressionFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProgressionFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ProgressionFragment newInstance(String param1, String param2) {
         ProgressionFragment fragment = new ProgressionFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_progression, container, false);
+        View RootView = inflater.inflate(R.layout.fragment_progression, container, false);
+
+
+        mapView = (MapView)RootView.findViewById(R.id.mapView2);
+        mapView.onCreate(savedInstanceState);
+
+        mapView.getMapAsync(this);
+
+        spinner = (Spinner) RootView.findViewById(R.id.spinner_journey);
+        btnShare = (Button) RootView.findViewById(R.id.button_share);
+        btnView = (Button) RootView.findViewById(R.id.button_view);
+        try {
+            db = new DBHandler(getActivity());
+            runs = db.getRunInstance();
+            for (int i = 0; i < runs.size(); i++) {
+                Log.d("Progression - Run", "Run " + Integer.toString(runs.get(i).get_id()) + " Date " + runs.get(i).get_date().toLocaleString());
+
+                if (spinner.getSelectedItem() == "Itinéraire 1") {
+                    index = 0;
+                } else if (spinner.getSelectedItem() == "Itinéraire 2") {
+                    index = 1;
+                } else if (spinner.getSelectedItem() == "Itinéraire 3") {
+                    index = 2;
+                }
+                locationList = runs.get(index).get_Points();
+
+            }
+
+            for (Location location : locationList) {
+                latLngList.add(new LatLng(location.getLatitude(), location.getLongitude()));
+            }
+        } catch(Exception e){
+            Log.d("Db","Impossible d'utiliser la db");
+        }
+        btnShare.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(v.getContext() , "Partage de l' "+ String.valueOf(spinner.getSelectedItem()),
+                        Toast.LENGTH_SHORT).show();
+                latLngList.add(new LatLng(0,0));
+                latLngList.add(new LatLng(1,1));
+                latLngList.add(new LatLng(2,2));
+                Share_data(latLngList);
+
+            }
+
+        });
+
+        btnView.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(v.getContext(), "Affichage de l' " + String.valueOf(spinner.getSelectedItem()),
+                        Toast.LENGTH_SHORT).show();
+                OnMapShow(latLngList);
+            }
+
+        });
+        return RootView;
     }
+
+    private boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public void Share_data( ArrayList<LatLng> latLngList){
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, latLngList.toString());
+        sendIntent.setType("text/plain");
+        startActivity(sendIntent);
+    }
+
+    public void OnMapShow (ArrayList<LatLng> latLngList) {
+        googleMap.addMarker(new MarkerOptions()
+                .position(latLngList.get(0))
+                .title("Départ"));
+        googleMap.addMarker(new MarkerOptions()
+                .position(latLngList.get(latLngList.size() - 1))
+                .title("Arrivée"));
+        for(int i=1; i< latLngList.size() - 1 ; i++)
+            googleMap.addMarker(new MarkerOptions()
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                    .alpha(0.3f)
+                    .position(latLngList.get(i)));
+        lineOptions = new PolylineOptions();
+        lineOptions.addAll(latLngList);
+        lineOptions.width(10);
+        lineOptions.color(Color.RED);
+        googleMap.addPolyline(lineOptions);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLngList.get(0)));
+    }
+
+
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
@@ -91,16 +214,51 @@ public class ProgressionFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+    @Override
+    public void onMapReady (GoogleMap Map) {
+        this.googleMap = Map;
+        googleMap.setTrafficEnabled(true);
+        googleMap.setIndoorEnabled(true);
+        googleMap.setBuildingsEnabled(true);
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+    @Override
+    public void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+    @Override
+    public void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
--- inf8405runner/app/src/main/java/com/example/pinor/inf8405runner/db/DBHandler.java
+++ inf8405runner/app/src/main/java/com/example/pinor/inf8405runner/db/DBHandler.java
@@ -5,9 +5,12 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.location.Location;
 import android.util.Log;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -30,6 +33,16 @@ public class DBHandler extends SQLiteOpenHelper {
     private static final String TXDEPART_COL_NAME = "tx_depart";
     private static final String RXDEPART_COL_NAME = "rx_depart";
 
+    private static final String TABLE_RUNS = "runs";
+    private static final String RU_ID_COL_NAME = "id";
+    private static final String RU_DATE_COL_NAME = "date";
+
+    private static final String TABLE_LOCATIONS = "locations";
+    private static final String L_ID_COL_NAME = "id";
+    private static final String L_LATITUDE_COL_NAME = "latitude";
+    private static final String L_LONGITUDE_COL_NAME = "longitude";
+    private static final String L_RUN_ID_COL_NAME = "run_id";
+
     public DBHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
@@ -50,13 +63,48 @@ public class DBHandler extends SQLiteOpenHelper {
         CREATE_DEVICE_TABLE += RXDEPART_COL_NAME + " INTEGER" + ")";
         Log.d("DB",CREATE_DEVICE_TABLE);
         db.execSQL(CREATE_DEVICE_TABLE);
+
+        this.createTableRun(db);
+        this.createTableLocations(db);
+    }
+
+    public void createTableRun(SQLiteDatabase db) {
+        String CREATE_TABLE_RUNS = "CREATE TABLE " + TABLE_RUNS + " (";
+        CREATE_TABLE_RUNS += RU_ID_COL_NAME + " INTEGER PRIMARY KEY, ";
+        CREATE_TABLE_RUNS += RU_DATE_COL_NAME + " INTEGER" + ")";
+        Log.d("DB",CREATE_TABLE_RUNS);
+        db.execSQL(CREATE_TABLE_RUNS);
+    }
+
+    public void createTableLocations(SQLiteDatabase db) {
+        String CREATE_TABLE_LOCATIONS = "CREATE TABLE " + TABLE_LOCATIONS + " (";
+        CREATE_TABLE_LOCATIONS += L_ID_COL_NAME + " INTEGER PRIMARY KEY, ";
+        CREATE_TABLE_LOCATIONS += L_LATITUDE_COL_NAME + " INTEGER, ";
+        CREATE_TABLE_LOCATIONS += L_LONGITUDE_COL_NAME + " INTEGER, ";
+        CREATE_TABLE_LOCATIONS += L_RUN_ID_COL_NAME + " INTEGER" + ")";
+        Log.d("DB",CREATE_TABLE_LOCATIONS);
+        db.execSQL(CREATE_TABLE_LOCATIONS);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_RESULTS);
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_DEVICE_INFO);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_RUNS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_LOCATIONS);
         onCreate(db);
+    }
+
+    private boolean checkIfTableExists(String tableName) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT DISTINCT name FROM sqlite_master WHERE name='" + tableName + "'";
+        Cursor cursor = db.rawQuery(query, null);
+        if (cursor.getCount() == 0) {
+            cursor.close();
+            return false;
+        }
+        cursor.close();
+        return true;
     }
 
     public void insertResult(Result result) {
@@ -109,7 +157,7 @@ public class DBHandler extends SQLiteOpenHelper {
     }
 
     public DeviceInfo getDeviceInfo() {
-        // List<DeviceInfo> deviceInfoList = new ArrayList<DeviceInfo>();
+       // List<DeviceInfo> deviceInfoList = new ArrayList<DeviceInfo>();
 
         String selectQuery = "SELECT * FROM " + TABLE_DEVICE_INFO;
         SQLiteDatabase db = this.getWritableDatabase();
@@ -125,8 +173,8 @@ public class DBHandler extends SQLiteOpenHelper {
             info.set_txDepart(cursor.getInt(2));
             info.set_rxDepart(cursor.getInt(3));
 
-            //deviceInfoList.add(info);
-            //} while (cursor.moveToNext());
+                //deviceInfoList.add(info);
+                //} while (cursor.moveToNext());
         }
         return info;
     }
@@ -134,6 +182,94 @@ public class DBHandler extends SQLiteOpenHelper {
     public void updateDeviceInfo(DeviceInfo info) {
         clearAllDeviceInfo();
         insertDeviceInfo(info);
+    }
+
+    public void insertRun(Run run) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        if (!checkIfTableExists(TABLE_RUNS)) {
+            createTableRun(db);
+        }
+
+        ContentValues row = new ContentValues();
+        row.put(RU_DATE_COL_NAME, run.get_date().getTime());
+        long runID = db.insert(TABLE_RUNS, null, row);
+
+        for (int i = 0; i < run.get_Points().size(); i++) {
+            insertLocation(runID, run.get_Points().get(i));
+        }
+
+        db.close();
+    }
+
+    public void insertLocation(long runID, Location location) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        if (!checkIfTableExists(TABLE_LOCATIONS)) {
+            createTableLocations(db);
+        }
+
+        ContentValues row = new ContentValues();
+        row.put(L_LATITUDE_COL_NAME, location.getLatitude());
+        row.put(L_LONGITUDE_COL_NAME, location.getLongitude());
+        row.put(L_RUN_ID_COL_NAME, runID);
+        db.insert(TABLE_LOCATIONS, null, row);
+        db.close();
+    }
+
+    public ArrayList<Run> getRunInstance() {
+        String selectQuery = "SELECT * FROM " + TABLE_RUNS;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        ArrayList<Run> runs = new ArrayList<>();
+
+        if (cursor.moveToFirst()) {
+            do {
+                Run run = new Run();
+                run.set_id(cursor.getInt(0));
+
+                Date date = new Date(cursor.getLong(1));
+                run.set_date(date);
+
+                runs.add(run);
+            } while (cursor.moveToNext());
+        }
+        return runs;
+    }
+
+    public ArrayList<Location> getLocations(int runID) {
+        String selectQuery = "SELECT * FROM " + TABLE_LOCATIONS;
+        selectQuery += " WHERE " + L_RUN_ID_COL_NAME + "=" + runID;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        ArrayList<Location> locations = new ArrayList<>();
+
+        if (cursor.moveToFirst()) {
+            do {
+                Location location = new Location("");
+                location.setLatitude(cursor.getDouble(1));
+                location.setLongitude(cursor.getDouble(2));
+
+                locations.add(location);
+            } while (cursor.moveToNext());
+        }
+        return locations;
+    }
+
+    public void clearAllLocationsAndRuns() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String deleteSQL = "DELETE FROM " + TABLE_RUNS;
+
+        if (checkIfTableExists(TABLE_RUNS)) {
+            db.execSQL(deleteSQL);
+        }
+
+        if (checkIfTableExists(TABLE_LOCATIONS)) {
+            deleteSQL = "DELETE FROM " + TABLE_LOCATIONS;
+            db.execSQL(deleteSQL);
+        }
     }
 }
 
